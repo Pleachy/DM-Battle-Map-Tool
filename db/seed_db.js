32a4323047f3123b04e59e7b6ec4705900b9c217
Pleachy <daniel.golub@gmail.com>
2021-12-01 21:14:38 -0800

    created users table

--- db/seed_db.js
+++ db/seed_db.js
@@ -5,6 +5,34 @@ const buildTables = async () => {
     try {
         client.connect();
 
+        //dropping tables
+        console.log('Dropping tables....');
+        await client.query(`
+        DROP TABLE IF EXISTS users
+        `)
+
+
+
+        //building tables
+        console.log('Creating tables....');
+        await client.query(`
+        
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            profileImage VARCHAR(255),
+            phoneNumber INT UNIQUE
+        );
+
+        CREATE TABLE characters
+        )
+
+
+        
+        `)
+
     } catch (error) {
         throw (error);
     }
