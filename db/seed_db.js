2d611a23c29a7c5194ae6349af2df89ebfd02257
Pleachy <daniel.golub@gmail.com>
2021-12-01 22:16:05 -0800

    tables for maps, campaigns and throughtables for users and maps added

--- db/seed_db.js
+++ db/seed_db.js
@@ -8,6 +8,10 @@ const buildTables = async () => {
         //dropping tables
         console.log('Dropping tables....');
         await client.query(`
+        DROP TABLE IF EXISTS campaign_maps;
+        DROP TABLE IF EXISTS maps;
+        DROP TABLE IF EXISTS campaign_users;
+        DROP TABLE IF EXISTS campaigns;
         DROP TABLE IF EXISTS characters;
         DROP TABLE IF EXISTS users;
         `)
@@ -40,8 +44,33 @@ const buildTables = async () => {
             "fortSave" INTEGER NOT NULL,
             "reflexSave" INTEGER NOT NULL,
             "willSave" INTEGER NOT NULL,
-            initiative INTEGER NOT NULL
+            initiative INTEGER NOT NULL,
+            "isPublic" BOOLEAN DEFAULT false
         );
+
+        CREATE TABLE campaigns (
+            id SERIAL PRIMARY KEY,
+            "dmId" INTEGER REFERENCES users(id)
+        );
+
+        CREATE TABLE campaign_users (
+            id SERIAL PRIMARY KEY,
+            "campaignId" INTEGER REFERENCES campaigns(id),
+            "userId" INTEGER REFERENCES users(id)
+        );
+
+        CREATE TABLE maps (
+            id SERIAL PRIMARY KEY,
+            "creatorId" INTEGER REFERENCES users(id)
+        );
+
+        CREATE TABLE campaign_maps (
+            id SERIAL PRIMARY KEY,
+            "campaignId" INTEGER REFERENCES campaigns(id),
+            "mapId" INTEGER REFERENCES maps(id)
+        );
+        
+
         `)
         console.log('Tables Created Successfully');
 
