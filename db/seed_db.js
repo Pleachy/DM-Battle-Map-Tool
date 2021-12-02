719c2f5f1ad6ede622c031a5ebc73e1e8ce714fb
Pleachy <daniel.golub@gmail.com>
2021-12-01 21:36:05 -0800

    created characters table

--- db/seed_db.js
+++ db/seed_db.js
@@ -8,7 +8,8 @@ const buildTables = async () => {
         //dropping tables
         console.log('Dropping tables....');
         await client.query(`
-        DROP TABLE IF EXISTS users
+        DROP TABLE IF EXISTS characters;
+        DROP TABLE IF EXISTS users;
         `)
         console.log('Tables dropped successfully');
 
@@ -23,10 +24,24 @@ const buildTables = async () => {
             username VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL,
-            profileImage VARCHAR(255),
-            phoneNumber INT UNIQUE
+            "profileImage" VARCHAR(255),
+            "phoneNumber" INTEGER UNIQUE
         );
 
+        CREATE TABLE characters (
+            id SERIAL PRIMARY KEY,
+            "userId" INTEGER REFERENCES users(id),
+            name VARCHAR(255) NOT NULL,
+            ac INTEGER NOT NULL,
+            cmb INTEGER NOT NULL,
+            cmd INTEGER NOT NULL,
+            ffac INTEGER NOT NULL,
+            tac INTEGER NOT NULL,
+            "fortSave" INTEGER NOT NULL,
+            "reflexSave" INTEGER NOT NULL,
+            "willSave" INTEGER NOT NULL,
+            initiative INTEGER NOT NULL
+        );
         `)
         console.log('Tables Created Successfully');
 
